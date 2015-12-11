969ada87ae61374096a303ff670d921dff25f706
glipecki <grzegorz.lipecki@gmail.com>
2015-12-11 21:29:10 +0100

    logging

--- sonarqube-companion-web/src/main/java/net/lipecki/sqcompanion/sonarqube/SonarQubeConnector.java
+++ sonarqube-companion-web/src/main/java/net/lipecki/sqcompanion/sonarqube/SonarQubeConnector.java
@@ -1,5 +1,7 @@
 package net.lipecki.sqcompanion.sonarqube;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
@@ -15,7 +17,10 @@ public class SonarQubeConnector {
     }
 
     public <T> ResponseEntity<T> getForEntity(final String action, final Class<T> responseClass) {
+        LOG.info("Request for: {}", url + action);
         return restTemplate.getForEntity(url + action, responseClass);
     }
+
+    private static final Logger LOG = LoggerFactory.getLogger(SonarQubeConnector.class);
 
 }
