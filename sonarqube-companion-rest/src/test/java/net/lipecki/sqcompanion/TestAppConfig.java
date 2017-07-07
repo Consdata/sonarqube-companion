72e42e1a34d58f34583a63f040e7b689ca352f78
Grzegorz Lipecki <grzegorz.lipecki@gmail.com>
2017-07-07 22:55:37 +0200

    @UtilityClass

--- sonarqube-companion-rest/src/test/java/net/lipecki/sqcompanion/TestAppConfig.java
+++ sonarqube-companion-rest/src/test/java/net/lipecki/sqcompanion/TestAppConfig.java
@@ -1,5 +1,6 @@
 package net.lipecki.sqcompanion;
 
+import lombok.experimental.UtilityClass;
 import net.lipecki.sqcompanion.config.AppConfig;
 import net.lipecki.sqcompanion.config.GroupDefinition;
 import net.lipecki.sqcompanion.config.ProjectLink;
@@ -8,32 +9,35 @@ import net.lipecki.sqcompanion.config.ProjectLinkType;
 /**
  * @author gregorry
  */
+@UtilityClass
 public class TestAppConfig {
 
-	public static AppConfig config() {
-		return AppConfig
-				.builder()
-				.rootGroup(
-						GroupDefinition.builder()
-								.projectLink(
-										ProjectLink.builder()
-												.link(TestAppConfig.RootGroup.Project1.KEY)
-												.type(ProjectLinkType.DIRECT)
-												.build()
-								)
-								.build()
-				)
-				.build();
-	}
+    public static AppConfig config() {
+        return AppConfig
+                .builder()
+                .rootGroup(
+                        GroupDefinition.builder()
+                                .projectLink(
+                                        ProjectLink.builder()
+                                                .link(TestAppConfig.RootGroup.Project1.KEY)
+                                                .type(ProjectLinkType.DIRECT)
+                                                .build()
+                                )
+                                .build()
+                )
+                .build();
+    }
 
-	public static class RootGroup {
+    @UtilityClass
+    public static class RootGroup {
 
-		public static class Project1 {
+        @UtilityClass
+        public static class Project1 {
 
-			public static final String KEY = "Project1";
+            public static final String KEY = "Project1";
 
-		}
+        }
 
-	}
+    }
 
 }
