19bca210c0ab310fa35df5ac0c70991752d2df6e
Grzegorz Lipecki <grzegorz.lipecki@gmail.com>
2017-07-30 12:04:27 +0200

    Wycięcie duplikatów przy sumowaniu tego samego projektu w wielu grupach #26

--- sonarqube-companion-rest/src/main/java/pl/consdata/ico/sqcompanion/repository/Project.java
+++ sonarqube-companion-rest/src/main/java/pl/consdata/ico/sqcompanion/repository/Project.java
@@ -3,6 +3,8 @@ package pl.consdata.ico.sqcompanion.repository;
 import lombok.Builder;
 import lombok.Data;
 
+import java.util.Objects;
+
 @Data
 @Builder
 public class Project {
@@ -20,6 +22,19 @@ public class Project {
                 .name(name)
                 .url(url)
                 .build();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        final Project project = (Project) o;
+        return Objects.equals(serverId, project.serverId) && Objects.equals(key, project.key);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(serverId, key);
     }
 
 }
