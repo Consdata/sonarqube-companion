51269bb9e1a3ec07fd36be3caf9615aa29d1c66e
Grzegorz Lipecki <grzegorz.lipecki@gmail.com>
2017-07-31 10:24:11 +0200

    Możliwość wyłączenia weryfikacji certyfikatów HTTPS. (#30)

---
+++ sonarqube-companion-rest/src/main/java/pl/consdata/ico/sqcompanion/RestTemplateConfiguration.java
@@ -0,0 +1,53 @@
+package pl.consdata.ico.sqcompanion;
+
+import org.apache.http.conn.ssl.NoopHostnameVerifier;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.conn.ssl.TrustSelfSignedStrategy;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.ssl.SSLContexts;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
+import sun.net.www.protocol.https.DefaultHostnameVerifier;
+
+import javax.net.ssl.SSLContext;
+
+@Configuration
+public class RestTemplateConfiguration {
+
+    @Bean
+    public RestTemplate restTemplate() {
+        if (allowSelfSignedHttps) {
+            return getRestTemplateAllowingSelfSigned();
+        } else {
+            return new RestTemplate();
+        }
+    }
+
+    private RestTemplate getRestTemplateAllowingSelfSigned() {
+        try {
+            final SSLContext allowSelfSignedSslContext = SSLContexts
+                    .custom()
+                    .loadTrustMaterial(null, new TrustSelfSignedStrategy())
+                    .build();
+            final SSLConnectionSocketFactory sslsf = new SSLConnectionSocketFactory(
+                    allowSelfSignedSslContext,
+                    NoopHostnameVerifier.INSTANCE
+            );
+            final CloseableHttpClient httpClient = HttpClientBuilder
+                    .create()
+                    .setSSLSocketFactory(sslsf)
+                    .build();
+            return new RestTemplate(new HttpComponentsClientHttpRequestFactory(httpClient));
+        } catch (final Exception exception) {
+            throw new SQCompanionException("Can't initialize RestTemplate with Self Signed https support", exception);
+        }
+    }
+
+    @Value("${app.allowSelfSignedHttps:false}")
+    private boolean allowSelfSignedHttps;
+
+}
