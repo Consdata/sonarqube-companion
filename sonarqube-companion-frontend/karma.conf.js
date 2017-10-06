28c40236285fe736e000063bb8773daa174542bb
Grzegorz Lipecki <grzegorz.lipecki@gmail.com>
2017-10-06 17:46:04 +0200

    podbicie zależności

--- sonarqube-companion-frontend/karma.conf.js
+++ sonarqube-companion-frontend/karma.conf.js
@@ -1,5 +1,5 @@
 // Karma configuration file, see link for more information
-// https://karma-runner.github.io/0.13/config/configuration-file.html
+// https://karma-runner.github.io/1.0/config/configuration-file.html
 
 module.exports = function (config) {
   config.set({
@@ -10,6 +10,7 @@ module.exports = function (config) {
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
+      require('karma-phantomjs-launcher'),
       require('@angular/cli/plugins/karma')
     ],
     client:{
