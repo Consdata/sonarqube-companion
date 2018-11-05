3751adc4eb62e2a2d805ea3ef725b0522323b56f
Grzegorz Lipecki <glipecki@consdata.pl>
2018-11-05 10:22:03 +0100

    users blacklist

--- sonarqube-companion-rest/src/main/java/pl/consdata/ico/sqcompanion/config/ServerDefinition.java
+++ sonarqube-companion-rest/src/main/java/pl/consdata/ico/sqcompanion/config/ServerDefinition.java
@@ -2,6 +2,10 @@ package pl.consdata.ico.sqcompanion.config;
 
 import lombok.Builder;
 import lombok.Data;
+import lombok.Singular;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Data
 @Builder
@@ -10,11 +14,16 @@ public class ServerDefinition {
     private String id;
     private String url;
     private ServerAuthentication authentication;
+    private List<String> blacklistUsers;
 
     public boolean hasAuthentication() {
         return authentication != null
                 && authentication.getType() != null
                 && !authentication.getType().isEmpty();
+    }
+
+    public List<String> getBlacklistUsers() {
+        return blacklistUsers != null ? blacklistUsers : new ArrayList<>();
     }
 
 }
--- sonarqube-companion-rest/src/main/java/pl/consdata/ico/sqcompanion/users/UsersService.java
+++ sonarqube-companion-rest/src/main/java/pl/consdata/ico/sqcompanion/users/UsersService.java
@@ -2,24 +2,32 @@ package pl.consdata.ico.sqcompanion.users;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import pl.consdata.ico.sqcompanion.config.AppConfig;
 import pl.consdata.ico.sqcompanion.sonarqube.SonarQubeFacade;
 import pl.consdata.ico.sqcompanion.sonarqube.SonarQubeUser;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Slf4j
 @Service
 public class UsersService {
 
     private final SonarQubeFacade sonarQubeFacade;
+    private final AppConfig appConfig;
 
-    public UsersService(final SonarQubeFacade sonarQubeFacade) {
+    public UsersService(final SonarQubeFacade sonarQubeFacade, AppConfig appConfig) {
         this.sonarQubeFacade = sonarQubeFacade;
+        this.appConfig = appConfig;
     }
 
     public List<SonarQubeUser> users(final String serverId) {
-        // TODO: blacklist for users
-        return sonarQubeFacade.users(serverId);
+        final List<String> blacklist = appConfig.getServer(serverId).getBlacklistUsers();
+        final List<SonarQubeUser> users = sonarQubeFacade.users(serverId)
+                .stream()
+                .filter(user -> blacklist.stream().noneMatch(blacklisted -> user.getUserId().matches(blacklisted)))
+                .collect(Collectors.toList());
+        return users;
     }
 
     public void sync() {
