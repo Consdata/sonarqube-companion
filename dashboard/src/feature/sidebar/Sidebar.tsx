fe98a0e1eb9acd343389e2ec5f578b1605f90e66
m87 <bartosz.radlinski@gmail.com>
2024-08-05 23:52:07 +0200

    sync stub

--- dashboard/src/feature/sidebar/Sidebar.tsx
+++ dashboard/src/feature/sidebar/Sidebar.tsx
@@ -28,7 +28,7 @@ export function Sidebar() {
     return (
         <div className="hidden border-r bg-muted/40 md:block">
             <div className="flex h-full max-h-screen flex-col gap-2">
-                <div className="flex h-14 items-center border-b px-4 min-h-14 lg:px-6">
+                <div className="flex h-14 items-center border-b px-4 min-h-14 lg:px-6  justify-between w-full">
                     <Building>
                     </Building>
                     {organizationInfoQuery.data?.name}
